474cb7dd4f03cf476c4567156d2d9d9813b2a28a
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2022-10-28 15:51:18 -0300

    Better sorting

--- packages/sorted.lua
+++ packages/sorted.lua
@@ -3,22 +3,93 @@ local base = require('packages.base')
 local package = pl.class(base)
 package._name = "sorted"
 
-local trimLeft = function (str)
+local trimLeft = function(str)
   return str:gsub("^%s*", "")
 end
 
-local trimRight = function (str)
+local trimRight = function(str)
   return str:gsub("%s*$", "")
 end
 
-local trim = function (str)
+local trim = function(str)
   return trimRight(trimLeft(str))
+end
+
+local function norm(str)
+  local tableAccents = {
+    ["À"] = "A",
+    ["Á"] = "A",
+    ["Â"] = "A",
+    ["Ã"] = "A",
+    ["Ä"] = "A",
+    ["Å"] = "A",
+    ["Æ"] = "AE",
+    ["Ç"] = "C",
+    ["È"] = "E",
+    ["É"] = "E",
+    ["Ê"] = "E",
+    ["Ë"] = "E",
+    ["Ì"] = "I",
+    ["Í"] = "I",
+    ["Î"] = "I",
+    ["Ï"] = "I",
+    ["Ð"] = "D",
+    ["Ñ"] = "N",
+    ["Ò"] = "O",
+    ["Ó"] = "O",
+    ["Ô"] = "O",
+    ["Õ"] = "O",
+    ["Ö"] = "O",
+    ["Ø"] = "O",
+    ["Ù"] = "U",
+    ["Ú"] = "U",
+    ["Û"] = "U",
+    ["Ü"] = "U",
+    ["Ý"] = "Y",
+    ["Þ"] = "P",
+    ["ß"] = "s",
+    ["à"] = "a",
+    ["á"] = "a",
+    ["â"] = "a",
+    ["ã"] = "a",
+    ["ä"] = "a",
+    ["å"] = "a",
+    ["æ"] = "ae",
+    ["ç"] = "c",
+    ["è"] = "e",
+    ["é"] = "e",
+    ["ê"] = "e",
+    ["ë"] = "e",
+    ["ì"] = "i",
+    ["í"] = "i",
+    ["î"] = "i",
+    ["ï"] = "i",
+    ["ð"] = "eth",
+    ["ñ"] = "n",
+    ["ò"] = "o",
+    ["ó"] = "o",
+    ["ô"] = "o",
+    ["õ"] = "o",
+    ["ö"] = "o",
+    ["ø"] = "o",
+    ["ù"] = "u",
+    ["ú"] = "u",
+    ["û"] = "u",
+    ["ü"] = "u",
+    ["ý"] = "y",
+    ["þ"] = "p",
+    ["ÿ"] = "y",
+  }
+
+  local normalisedString = ''
+  local normalisedString = str:gsub("[%z\1-\127\194-\244][\128-\191]*", tableAccents)
+  return normalisedString
 end
 
 function package:registerCommands()
   local items = {}
 
-  self:registerCommand("sort", function (options, content)
+  self:registerCommand("sort", function(options, content)
     local sep = options.sep or ', '
 
     if #content == 1 and content.command == "sort" then
@@ -31,11 +102,11 @@ function package:registerCommands()
 
     local items = pl.stringx.split(content[1], sep)
 
-    table.sort(items, function (a, b)
-      return a:lower() < b:lower()
+    table.sort(items, function(a, b)
+      return norm(a):lower() < norm(b):lower()
     end)
 
-    for i = 1,items:len() do
+    for i = 1, items:len() do
       SILE.typesetter:typeset(items[i])
 
       if i ~= #items then
@@ -44,12 +115,12 @@ function package:registerCommands()
     end
   end)
 
-  self:registerCommand("sorted", function (options, content)
+  self:registerCommand("sorted", function(options, content)
     local items = {}
 
     for i = 1, #content do
       if type(content[i]) == "table" and content[i].command == "item" then
-        items[#items+1] = {
+        items[#items + 1] = {
           key = content[i].options.key or i,
           content = SU.subContent(content[i]),
         }
@@ -60,17 +131,18 @@ function package:registerCommands()
         else
           text = content[i].command
         end
-        if text ~= "" then SU.warn("Ignored \""..text.."\"") end
+        if text ~= "" then SU.warn("Ignored \"" .. text .. "\"") end
       end
     end
 
-    table.sort(items, function (a, b)
-      local x = tostring(a.key)
-      local y = tostring(b.key)
+    table.sort(items, function(a, b)
+      local x = norm(tostring(a.key))
+      local y = norm(tostring(b.key))
 
       return x:lower() < y:lower()
     end)
 
+    SILE.typesetter:leaveHmode()
     for i = 1, #items do
       if options['pre-key'] then
         SILE.call(options['pre-key'], items[i], { items[i].key })
@@ -85,7 +157,7 @@ function package:registerCommands()
         SILE.call(options['post-content'], items[i], items[i].content)
       end
       if options['post-key'] then
-        SILE.call(options['post-key'], items[i], { items[i].content })
+        SILE.call(options['post-key'], items[i], { items[i].key })
       end
 
       SILE.typesetter:leaveHmode()
